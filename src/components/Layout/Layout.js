b626ec76332409babefb10f65c2fb6bce55b701d
Mike Brown <mpbrown@buffalo.edu>
2020-10-27 11:10:37 -0400

    Add burger, ingredients, and controls

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import styles from "./Layout.module.css";
+
+const layout = (props) => {
+    return (
+        <Fragment>
+            <div>Toolbar, SideDrawer, Backdrop</div>
+            <main className={styles.Content}>{props.children}</main>
+        </Fragment>
+    );
+};
+
+export default layout;
